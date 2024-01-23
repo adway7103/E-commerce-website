114dae2b1b6d61f5110b483a61d141ea18e8ee81
Adway Lachhiramka <66119557+adway7103@users.noreply.github.com>
2024-01-23 22:58:25 +0530

    s

--- digitalbuddy/src/app/page.tsx
+++ digitalbuddy/src/app/page.tsx
@@ -3,6 +3,7 @@ import { buttonVariants } from "@/components/ui/button";
 import Image from "next/image";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
+import { gsap } from "gsap";
 export default function Home() {
   return (
     <MaxWidthWrapper>
